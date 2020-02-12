1a1a28f1ccfadb5513509d25dcaa9a16e119ec94
Bruno <bmdefreitas@gmail.com>
2020-02-11 21:54:21 -0300

    Helmet, Cors, Compression

--- app.js
+++ app.js
@@ -1,25 +1,28 @@
-const createError = require("http-errors");
-const express = require("express");
-const cookieParser = require("cookie-parser");
-const logger = require("morgan");
-const helmet = require("helmet");
-const cors = require("cors");
+const createError = require('http-errors');
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
+const compression = require('compression');
 
-const indexRouter = require("./src/routes/index");
-const apiRouter = require("./src/routes/api");
+const indexRouter = require('./src/routes/index');
+const apiRouter = require('./src/routes/api').default;
 
 const app = express();
 
-//app.use(cors);
-//app.use(helmet);
+app.use(cors());
+app.use(helmet());
 
-app.use(logger("dev"));
+app.use(compression());
+
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use("/", indexRouter);
-app.use("/api", apiRouter);
+app.use('/', indexRouter);
+app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
@@ -27,14 +30,14 @@ app.use((req, res, next) => {
 });
 
 // error handler
-app.use((err, req, res, next) => {
+app.use((err, req, res) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render("error");
+  res.render('error');
 });
 
 module.exports = app;
